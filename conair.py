903f7a906e8b0b5711f62eb504415e665ddbf406
Sam Davies <sam.jc.davies@gmail.com>
2012-09-13 22:44:40 +0100

    Updates conair.py to push data to TempoDB
    Well, it hasn't been tested, but I think that this is what it should do.
    Will need to remove the API key and secret at a future point.

--- conair.py
+++ conair.py
@@ -1,12 +1,17 @@
 import serial
 import math
+import datetime
+import random
+from tempodb import Client, DataPoint
+
+client = Client('cc0c654d01774b128c1e0495de51784b', 'a280c43f6b27400998a4aba0b1eb4545')
 
 def calculate_temp(value):
     voltage = float(value) / 1024 * 5
     resistance = 10000 / (5 / voltage - 1)
     temp = 1 / (1/298.15 + math.log(resistance / 10000) / 3977)
     print "Temperature is: %f K, (%f degC)" % (temp, temp-273.15)
-    return
+    return temp-273.15
 
 ser = serial.Serial('/dev/tty.usbserial-A800etDk', 9600)
 while 1:
@@ -14,5 +19,6 @@ while 1:
     split = r.split(": ")
     if split[0] == "sensorValue":
         value = split[1].strip()
-        calculate_temp(value)
+        temp = calculate_temp(value)
+        client.write_key('temperature', DataPoint(datetime.datetime.now(), temp))
 
