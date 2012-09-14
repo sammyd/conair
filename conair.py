7b22326f7e81779c60764287237d4a4901e44373
Sam Davies <sam.jc.davies@gmail.com>
2012-09-14 23:25:25 +0100

    Pulls out some constants so to make it a bit easier to understand
    This is the commit around which the first blog post was based

--- conair.py
+++ conair.py
@@ -4,12 +4,17 @@ import datetime
 import random
 from tempodb import Client, DataPoint
 
+POTENTIAL_DIVIDER_RESISTOR = 10000
+THERMISTOR_B_VALUE = 3977
+THERMISTOR_REF_TEMP = 298.15
+THERMISTOR_REF_RESISTANCE = 10000
+
 client = Client('cc0c654d01774b128c1e0495de51784b', 'a280c43f6b27400998a4aba0b1eb4545')
 
 def calculate_temp(value):
     voltage = float(value) / 1024 * 5
-    resistance = 10000 / (5 / voltage - 1)
-    temp = 1 / (1/298.15 + math.log(resistance / 10000) / 3977)
+    resistance = POTENTIAL_DIVIDER_RESISTOR / (5 / voltage - 1)
+    temp = 1 / (1/THERMISTOR_REF_TEMP + math.log(resistance / THERMISTOR_REF_RESISTANCE) / THERMISTOR_B_VALUE)
     print "Temperature is: %f K, (%f degC)" % (temp, temp-273.15)
     return temp-273.15
 
