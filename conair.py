ccff9c765dd52a8453eb4ba0496f32ffb193d69a
Sam Davies <sam.jc.davies@gmail.com>
2013-07-02 07:49:42 +0100

    Updates the script to match new serial input

--- conair.py
+++ conair.py
@@ -22,9 +22,10 @@ ser = serial.Serial('/dev/tty.usbserial-A800etDk', 9600)
 temperature_array = []
 while 1:
     r = ser.readline()
-    split = r.split(" = ")
-    if split[0] == "sensorValue0":
-        value = split[1].strip()
+    split = r.split(",")
+    if(len(split) == 3):
+        # Deal with the temperature pin first
+        value = split[0].strip()
         temp = calculate_temp(value)
         temperature_array.append(temp)
         if(len(temperature_array) >= 20):
@@ -32,9 +33,14 @@ while 1:
             print "Saving off this minute's mean: %f" % mean
             client.write_key('temperature', [DataPoint(datetime.datetime.utcnow(), mean)])
             temperature_array = []
-    elif split[0] == "sensorValue1":
+
+        # And now the LDR resistance
         value = split[1].strip()
         voltage = float(value) / 1024 * 5
         resistance = POTENTIAL_DIVIDER_RESISTOR / (5 / voltage - 1)
         print "LDR resistance is: %f Ohms" % resistance
+
+        # And finally, the humidity
+        value = split[2].strip()
+        print "Humidity reading is: %d" % float(value)
 
